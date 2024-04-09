2e3f5c8d4e9dfc6cc0787474230b990b3a7bca3e
0xdwong <0xdwong@gmail.com>
2024-04-09 16:37:05 +0800

    update: intro

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -27,25 +27,7 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
           Welcome to Solaigent!
         </h1>
         <p className="leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink>, the{' '}
-          <ExternalLink href="https://sdk.vercel.ai">
-            Vercel AI SDK
-          </ExternalLink>
-          , and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
-        </p>
-        <p className="leading-normal text-muted-foreground">
-          It uses{' '}
-          <ExternalLink href="https://vercel.com/blog/ai-sdk-3-generative-ui">
-            React Server Components
-          </ExternalLink>{' '}
-          to combine text with generative UI as output of the LLM. The UI state
-          is synced through the SDK so the model is aware of your interactions
-          as they happen.
+          Solaigen is an AI agent based on Solana, aimed at providing users with various functions and services. The project will combine artificial intelligence and blockchain technology to provide users with efficient, intelligent solutions.
         </p>
       </div>
     </div>
